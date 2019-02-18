c41091d1855026a885f01c5d4e320f03bbf140a8
courtneywilliams561@gmail.com <36859589+kwame561@users.noreply.github.com>
2019-02-17 23:20:18 -0500

    Updated States...

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Photo from './Photo'
 
-class PhotoWall extends Component {
-    render() {
+function PhotoWall(props) {
         return <div className="photo-grid">
-                {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+                {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
              </div>
-    }
 }
 
 export default PhotoWall
