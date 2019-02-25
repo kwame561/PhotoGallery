57cb2054c4ae9c7a8e3b4cc76c9140cfa3e8d652
courtneywilliams561@gmail.com <36859589+kwame561@users.noreply.github.com>
2019-02-24 21:50:17 -0500

    Added Redux to make state management more predictable

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,76 +2,35 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
+import {removePost} from '../redux/actions'
 
 class Main extends Component {
     constructor() {
         super()
         // the below property is use for state management for each posts
-        this.state = {
-            posts: [
-                {
-                    id: 0,
-                    description: "Spiderman",
-                    imageLink: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/01/df-28509_r2_r-h_2017.jpg"
-                }, {
-                    id: 1,
-                    description: "Mindfulness",
-                    imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqdoI48yvKHGyWM8KkoTiLZZWhNPqnmX3BYB6QFqwlhoqZG-nO"
-                }, {
-                    id: 2,
-                    description: "Day at the Museum",
-                    imageLink: require('../images/museum.jpg')
-                }
-                , {
-                    id: 3,
-                    description: "Downtown",
-                    imageLink: require('../images/downtown.jpg')
-                }
-            ]
-        }
-        this.removePhoto = this.removePhoto.bind(this)
+        
     }
 
-    removePhoto(postRemoved) {
-        console.log(postRemoved.description)
-        this.setState((state) => ({
-            posts: state.posts.filter(post => post !== postRemoved)
-        }))
-    }
-
-    addPhoto(postSubmitted) {
-        this.setState(state => ({
-            posts: state.posts.concat([postSubmitted])
-        }))
-    }
-
-    // lifecycle component evoked
-    componentDidMount() {
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log(prevState.posts)
-        console.log(this.state)
-    }
+    // componentDidMount() {
+    //     this.props.removePost(1)
+    // }
 
     render() {
-        console.log(this.state.posts)
+        console.log(this.props)
         return (
                 <div>
+                    <h1>
+                        <Link to="/">Courtney's Photowall</Link>
+                    </h1>
                     <Route exact path="/" render={() => (
                         <div>
-                            <Title title = {"Courtney's Photowall"}/>
-                            <PhotoWall posts = {this.state.posts} onRemovePhoto={this.removePhoto} onNavigate = {this.navigate}/>
+                            <PhotoWall {...this.props}/>
                         </div>
                     )}/>
                     <Route path="/AddPhoto" render={({history}) => (
-                        <AddPhoto onAddPhoto={(addedPost) => {
-                            this.addPhoto(addedPost)
-                            history.push("/")
-                        }}/>
-                    )}
-                    />
+                        <AddPhoto {...this.props} onHistory={history}/> 
+                    )}/>
                 </div>
         )
     }
---
+++ src/data/posts.js
@@ -0,0 +1,22 @@
+const posts = [
+    {
+        id: 0,
+        description: "Spiderman",
+        imageLink: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/01/df-28509_r2_r-h_2017.jpg"
+    }, {
+        id: 1,
+        description: "Mindfulness",
+        imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqdoI48yvKHGyWM8KkoTiLZZWhNPqnmX3BYB6QFqwlhoqZG-nO"
+    }, {
+        id: 2,
+        description: "Day at the Museum",
+        imageLink: require('../images/museum.jpg')
+    }
+    , {
+        id: 3,
+        description: "Downtown",
+        imageLink: require('../images/downtown.jpg')
+    }
+]
+
+export default posts
