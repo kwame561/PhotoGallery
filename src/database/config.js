11548b91583753519546bf4bc5058d98e6d888a3
courtneywilliams561@gmail.com <36859589+kwame561@users.noreply.github.com>
2019-03-13 22:54:29 -0400

    added the .env file

--- src/database/config.js
+++ src/database/config.js
@@ -1,17 +1,21 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAuRdR69OmJu-iECQ5DF8z6mT3aYCtC-Nw",
-  authDomain: "photowall-ea942.firebaseapp.com",
-  databaseURL: "https://photowall-ea942.firebaseio.com",
-  projectId: "photowall-ea942",
-  storageBucket: "photowall-ea942.appspot.com",
-  messagingSenderId: "431928077249"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
-firebase.initializeApp(config)
+
+firebase.initializeApp(config);
+
 
 const database = firebase.database()
+const storage = firebase.storage()
 
-export {database}
+export {database, storage};
